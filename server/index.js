bd83c3a81fc8e166369d358f5c7f459ca6263281
Francesco Commisso <francescocommissodev@gmail.com>
2019-07-11 15:59:49 -0400

    initial commit

---
+++ server/index.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const pino = require("express-pino-logger")();
+const path = require("path");
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json({ extended: false }));
+app.use(pino);
+app.use(express.static(path.join(__dirname, "../client/build")));
+
+const port = process.env.PORT || 3001;
+app.listen(port, () => {
+  console.log("Express server is running on localhost:3001");
+});
+
+app.get("*", function(req, res) {
+  res.sendFile(path.join(__dirname, "../client/build/index.html"), function(
+    err
+  ) {
+    if (err) {
+      res.status(500).send(err);
+    }
+  });
+});
